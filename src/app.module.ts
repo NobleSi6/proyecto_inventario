f94e93b4f2543639eebe7242d040553465be0aa0
Gamerlan7650 <alanmaldonadoc5@gmail.com>
2025-10-20 15:22:02 -0400

    primera prueba de endpoints dos tablas

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@
 
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+//estan comentados para evitar errores si no existen los modulos
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { AuthModule } from './auth/auth.module';
 import { InventarioModule } from './inventario/inventario.module';
+
+import { SalidasModule } from './salidas_inventario/salidas.module';
+import { DetallesSalidaModule } from './detalle_salida/detalles-salida.module';
+
+import { Salida } from './salidas_inventario/salida.entity';
+import { DetalleSalida } from './detalle_salida/detalle-salida.entity'
+
 
 @Module({
   imports: [
@@ -18,15 +27,20 @@ import { InventarioModule } from './inventario/inventario.module';
       database: 'testbackup', // El nombre de tu DB
       
       // Importante: lista todas tus entidades aquí
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      entities: [Salida, DetalleSalida], // Agrega tus entidades aquí
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'], "descomentar luego de las pruebas"
       
       synchronize: true, // ¡Solo usar en desarrollo! Esto crea las tablas automáticamente.
       autoLoadEntities: true, // Carga automáticamente las entidades
     }),
     
-    UsuariosModule,
-    AuthModule,
-    InventarioModule,
+    //UsuariosModule,
+    //AuthModule,
+    //InventarioModule,
+
+    SalidasModule,
+    DetallesSalidaModule,
+
     // ... otros módulos
   ],
   controllers: [],
