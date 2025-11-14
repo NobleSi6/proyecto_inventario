5c4fab992e78af846b694190b1b0a6a2b0b2cf76
AlanMC7650 <alan.maldonado@ucb.edu.bo>
2025-11-14 13:20:31 -0400

    Creacion ed un microservicio Core para el manejo de inventarios

--- src/detalle_orden_compra/dto/create-detalle_orden_compra.dto.ts
+++
@@ -1,9 +0,0 @@
-export class CreateDetalleOrdenCompraDto {
-  id_orden_compra: number;
-  id_material: number;
-  cantidad: number;
-  precio_unitario: number;
-  subtotal: number;
-  observaciones?: string;
-  activo: boolean;
-}
